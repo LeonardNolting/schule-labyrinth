71f8b490d58b0a78c913e483a0890398f57b3497
LeonardNolting <49598438+LeonardNolting@users.noreply.github.com>
2021-12-18 13:40:16 +0100

    so ziemlich alles

--- src/index.ts
+++ src/index.ts
@@ -2,54 +2,48 @@ import {JSDOM} from 'jsdom'
 import {readdirSync, readFileSync, statSync} from 'fs'
 import * as path from "path"
 
-interface Struktur {
-	dateien: string[]
-	stylesheets: string[]
-	ordner: {[name: string]: Struktur}
-}
-
-interface StrukturFertig {
-	[pfad: string]: string | StrukturFertig
-}
-
-const ladeStruktur = (ordner: string) => readdirSync(ordner).reduce<Struktur>((struktur, item) => {
-	const stats = statSync(path.resolve(ordner, item))
-	if (stats.isDirectory()) struktur.ordner[item] = ladeStruktur(path.resolve(ordner, item))
-	else if (stats.isFile()) {
-		const dateiEndung = path.extname(item).toLowerCase()
-		if (dateiEndung === "css") struktur.stylesheets.push(item)
-		else if (dateiEndung === "html") struktur.dateien.push(item)
-	}
-	return struktur
-}, {dateien: [], stylesheets: [], ordner: {}});
-
-function baueDatei(pfad: string, stylesheets: string[]): string {
-	/*JSDOM.fromFile(ordner + "/" + datei).then((dom) => {
-		dom.window.document.head.innerHTML += "<style>body { background: red; }</style>"
-		console.log(dom.serialize());
-	})*/
-	return ""
-}
-
- function baueStylesheet(pfad: string): string {
-	return readFileSync(pfad).toString()
- }
-
-function baueStrukturFertig(ordner: string, struktur: Struktur, stylesheets: string[] = []): StrukturFertig {
-	const generiert: StrukturFertig = {}
-	stylesheets.push(...struktur.stylesheets.map(stylesheet => baueStylesheet(path.resolve(ordner, stylesheet))))
-	struktur.dateien.forEach(datei => {
-		generiert[datei] = baueDatei(path.resolve(ordner, datei), stylesheets);
-	});
-	Object.entries(struktur.ordner).forEach(([kindOrdner, kindStruktur]) => {
-		generiert[kindOrdner] = baueStrukturFertig(path.resolve(ordner, kindOrdner), kindStruktur, [...stylesheets])
+const baueOrdner = (ordner: string): { dateien: string[], stylesheets: string[], ordner: string[] } => {
+	const struktur = {dateien: [], stylesheets: [], ordner: []}
+	readdirSync(ordner).forEach(item => {
+		const pfad = path.join(ordner, item)
+		const stats = statSync(pfad)
+		if (stats.isDirectory()) struktur.ordner.push(item)
+		else if (stats.isFile()) {
+			const dateiEndung = path.extname(item).toLowerCase().slice(1)
+			if (dateiEndung === "css") struktur.stylesheets.push(item)
+			else if (dateiEndung === "html") struktur.dateien.push(item)
+		}
 	})
+	return struktur
+};
+
+async function baueDatei(pfad: string, stylesheets: string[]): Promise<string> {
+	const dom = await JSDOM.fromFile(pfad)
+	stylesheets.forEach(stylesheet => dom.window.document.head.innerHTML += `<style>${stylesheet}</style>`)
+	return dom.serialize()
+}
+
+const baueStylesheet = (pfad: string) => readFileSync(pfad).toString();
+
+const dateien = async (ordner: string, stylesheets: string[]): Promise<{ [pfad: string]: string }> => {
+	const ordnerAnalyse = baueOrdner(ordner)
+	const generiert = {}
+	stylesheets.push(...ordnerAnalyse.stylesheets.map(stylesheet => baueStylesheet(path.join(ordner, stylesheet))))
+	for (const datei of ordnerAnalyse.dateien) {
+		const pfad = path.join(ordner, datei)
+		generiert[pfad] = await baueDatei(pfad, stylesheets);
+	}
+	for (const name of ordnerAnalyse.ordner)
+		Object.assign(generiert, await dateien(path.join(ordner, name), stylesheets))
 	return generiert
 }
 
-function bauen(input: string, output: string) {
-	const struktur = ladeStruktur(input)
-	baueStrukturFertig(".", struktur)
+async function bauen(input: string, output: string) {
+	Object.entries(await dateien(input, [])).forEach(([pfad, inhalt]) => {
+		console.log()
+		console.log(pfad, inhalt)
+	})
 }
 
-bauen("./input", "./output")
+bauen("./input", "./output").then(() => {
+})
