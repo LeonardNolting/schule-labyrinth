242cc1d4e32c8c7d60f988ce8d9d874e945188bd
LeonardNolting <49598438+LeonardNolting@users.noreply.github.com>
2021-12-18 11:53:18 +0100

    bauenStrukturFertig

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import {JSDOM}       from 'jsdom'
-import {readdirSync, statSync} from 'fs'
-import * as path          from "path"
+import {JSDOM} from 'jsdom'
+import {readdirSync, readFileSync, statSync} from 'fs'
+import * as path from "path"
 
 interface Struktur {
 	dateien: string[]
@@ -8,7 +8,11 @@ interface Struktur {
 	ordner: {[name: string]: Struktur}
 }
 
-const ladeStruktur = ordner => readdirSync(ordner).reduce<Struktur>((struktur, item) => {
+interface StrukturFertig {
+	[pfad: string]: string | StrukturFertig
+}
+
+const ladeStruktur = (ordner: string) => readdirSync(ordner).reduce<Struktur>((struktur, item) => {
 	const stats = statSync(path.resolve(ordner, item))
 	if (stats.isDirectory()) struktur.ordner[item] = ladeStruktur(path.resolve(ordner, item))
 	else if (stats.isFile()) {
@@ -19,17 +23,33 @@ const ladeStruktur = ordner => readdirSync(ordner).reduce<Struktur>((struktur, i
 	return struktur
 }, {dateien: [], stylesheets: [], ordner: {}});
 
-function verarbeiteStruktur(struktur: Struktur) {
-
-}
-
-// function erstelleDatei(pfad, stylesheets)
-
-function leseDatei (ordner, datei) {
-	JSDOM.fromFile(ordner + "/" + datei).then((dom) => {
+function baueDatei(pfad: string, stylesheets: string[]): string {
+	/*JSDOM.fromFile(ordner + "/" + datei).then((dom) => {
 		dom.window.document.head.innerHTML += "<style>body { background: red; }</style>"
 		console.log(dom.serialize());
-	})
+	})*/
+	return ""
 }
 
-ladeStruktur("./input")
+ function baueStylesheet(pfad: string): string {
+	return readFileSync(pfad).toString()
+ }
+
+function baueStrukturFertig(ordner: string, struktur: Struktur, stylesheets: string[] = []): StrukturFertig {
+	const generiert: StrukturFertig = {}
+	stylesheets.push(...struktur.stylesheets.map(stylesheet => baueStylesheet(path.resolve(ordner, stylesheet))))
+	struktur.dateien.forEach(datei => {
+		generiert[datei] = baueDatei(path.resolve(ordner, datei), stylesheets);
+	});
+	Object.entries(struktur.ordner).forEach(([kindOrdner, kindStruktur]) => {
+		generiert[kindOrdner] = baueStrukturFertig(path.resolve(ordner, kindOrdner), kindStruktur, [...stylesheets])
+	})
+	return generiert
+}
+
+function bauen(input: string, output: string) {
+	const struktur = ladeStruktur(input)
+	baueStrukturFertig(".", struktur)
+}
+
+bauen("./input", "./output")
